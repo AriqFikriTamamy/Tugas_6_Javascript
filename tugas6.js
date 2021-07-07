f7fb4afbd288eda7fa28d92e4eb1a7929e48b880
Ariq Fikri Tamamy <ariqrick@gmail.com>
2021-07-07 18:46:01 +0700

    Tugas 6 Javascript

---
+++ tugas6.js
@@ -0,0 +1,28 @@
+// function rekursif() {
+//   for (var i = 0; i <= 20; i++) {
+//     console.log()
+//     }
+//
+// rekursif()
+
+// var i = 20
+//
+// do {
+//   console.log("satu",i)
+// } while (i == 1);
+
+for (var i = 1; i <= 20; i++) {
+  if (i%2===0) {
+    console.log("bilangan genap")
+  }else {
+    console.log(i)
+  }
+}
+
+// for(var i=1; i<=20;i++){
+//   if(i%2===0){
+//     console.log("adalah bilangan genap")
+//   }else{
+//     console.log(i)
+//   }
+// }
